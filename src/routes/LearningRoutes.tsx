0d9b5778bec0127ffad02d335867c2781d4dab83
eri-kawabata <eri.092127.k@gmail.com>
2025-03-20 15:59:12 +0900

    削除

--- src/routes/LearningRoutes.tsx
+++ src/routes/LearningRoutes.tsx
@@ -8,22 +8,12 @@ import { ArtLearning } from '../components/Learning/ArtLearning';
 import { MathLearning } from '../components/Learning/MathLearning';
 import { LessonDetail } from '../components/Learning/LessonDetail';
 import { LessonContent } from '../components/Learning/LessonContent';
-import { Biology } from '../pages/child/learning/science/Biology';
-import { Plant } from '../pages/child/learning/science/Plant';
-import { Weather } from '../pages/child/learning/science/Weather';
-import { Earth } from '../pages/child/learning/science/Earth';
-import { Matter } from '../pages/child/learning/science/Matter';
 
 function LearningRoutes() {
   return (
     <Routes>
       <Route index element={<Learning />} />
       <Route path="science" element={<ScienceLearning />} />
-      <Route path="science/biology" element={<Biology />} />
-      <Route path="science/plant" element={<Plant />} />
-      <Route path="science/weather" element={<Weather />} />
-      <Route path="science/earth" element={<Earth />} />
-      <Route path="science/matter" element={<Matter />} />
       <Route path="technology" element={<TechnologyLearning />} />
       <Route path="engineering" element={<EngineeringLearning />} />
       <Route path="art" element={<ArtLearning />} />
